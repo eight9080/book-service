6eea576f7d29cddbe2b2a07ee5e7a3493099a5ad
eight9080 <eight9080@gmail.com>
2018-08-28 21:21:14 +0300

    test2

--- src/main/java/ro/dg/bookservice/BookServiceApplication.java
+++ src/main/java/ro/dg/bookservice/BookServiceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BookServiceApplication {
 
 	public String message;
+	public String message2;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookServiceApplication.class, args);
